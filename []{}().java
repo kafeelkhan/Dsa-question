3b7e185f69e32c818adac238901cb0bf3f4bfcf3
kafeelkhan <kkafeel538@gmail.com>
2021-07-20 10:28:11 +0530

    initail commit

---
+++ []{}().java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class []{}(){
+public static void main(String [] arg){
+	Scanner sc=new Scanner(System.in);
+	String string;
+    System.out.println("Enter The String ");
+    string =sc.nextLine();
+    System.out.println("Name : "+string);
+    
+
+
+}
+}
