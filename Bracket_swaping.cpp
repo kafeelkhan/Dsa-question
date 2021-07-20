2f0dca353f5ea4ed05a75cbaec1116ab1b239d95
kafeelkhan <kkafeel538@gmail.com>
2021-07-20 10:20:15 +0530

    initial commit

---
+++ Bracket_swaping.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	cin>>str;
+	int imbalance=0;
+	int left=0,right=0,sum=0;
+	for(int i=0;i<str.length();i++)
+    { if(str[i]=='['){
+        left++;
+           if(imbalance>0){
+              sum+=imbalance;
+              imbalance--;
+           }
+       }
+       else if(str[i]==']'){
+           right++;
+           imbalance=right-left;
+           
+       }
+       
+    }	
+	cout<<"Number of Swap : "<<sum;
+    return 0;
+   
+}
